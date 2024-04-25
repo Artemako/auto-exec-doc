47a2b86ed0115182d7d355a3ae9739287602fb85
Artemako <hayartemako@yandex.ru>
2024-04-25 20:30:57 +0300

    пытки с компиляцей

--- main.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=C:\Users\hayar\AppData\Local\Programs\Python\PYTHON~1;%PATH%
-set PYTHONHOME=C:\Users\hayar\AppData\Local\Programs\Python\Python311
-set NUITKA_PYTHONPATH=D:\work\project\AutoExecDoc;C:\Users\hayar\AppData\Local\Programs\Python\Python311\DLLs;C:\Users\hayar\AppData\Local\Programs\Python\Python311\Lib;C:\Users\hayar\AppData\Local\Programs\Python\Python311;D:\work\project\AutoExecDoc\venv;D:\work\project\AutoExecDoc\venv\Lib\site-packages;D:\work\project\AutoExecDoc\venv\Lib\site-packages\win32;D:\work\project\AutoExecDoc\venv\Lib\site-packages\win32\lib;D:\work\project\AutoExecDoc\venv\Lib\site-packages\Pythonwin
-"%~dp0main.exe" %*
